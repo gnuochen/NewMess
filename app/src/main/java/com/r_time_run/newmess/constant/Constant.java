d3d5d59b68f84d274aae9e07e32e96bf0cfcfd38
nuochen <1787699825@qq.com>
2015-11-26 23:37:18 +0800

    诺晨 11.26 修改   测试了下服务器，妈的，tomcat坏了

--- app/src/main/java/com/r_time_run/newmess/constant/Constant.java
+++ app/src/main/java/com/r_time_run/newmess/constant/Constant.java
@@ -5,5 +5,7 @@ package com.r_time_run.newmess.constant;
  */
 public class Constant {
     public static final String url = "http://115.29.55.71:8080/NewMessServlet/";
-    public final static String URL_FOODS = url+"servlet/NMServlet";
+    public final static String URL_FOODS = url + "servlet/NMServlet";
+//    public static final String url = "http://192.168.1.114:8080/NewMess/";
+//    public final static String URL_FOODS = url+"servlet/ClientSide";
 }
