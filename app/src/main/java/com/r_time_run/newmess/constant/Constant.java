e591e0c157a10a0ec90471b85901289d266f5743
nuochen <1787699825@qq.com>
2015-11-28 15:38:54 +0800

    诺晨 11.28 修改   测试本地服务器

--- app/src/main/java/com/r_time_run/newmess/constant/Constant.java
+++ app/src/main/java/com/r_time_run/newmess/constant/Constant.java
@@ -4,8 +4,9 @@ package com.r_time_run.newmess.constant;
  * Created by nuochen on 2015/9/16.
  */
 public class Constant {
-    public static final String url = "http://115.29.55.71:8080/NewMessServlet/";
-    public final static String URL_FOODS = url + "servlet/NMServlet";
-//    public static final String url = "http://192.168.1.114:8080/NewMess/";
-//    public final static String URL_FOODS = url+"servlet/ClientSide";
+//    public static final String url = "http://115.29.55.71:8080/NewMessServlet/";
+//    public final static String URL_FOODS = url + "servlet/NMServlet";
+
+    public static final String url = "http://192.168.1.112:8080/NewMess/";
+    public final static String URL_FOODS = url+"servlet/ClientSide";
 }
