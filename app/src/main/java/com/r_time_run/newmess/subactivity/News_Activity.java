5fbd03e870faeb1713b8043fd6a61668160fa544
cjj <812371098@qq.com>
2015-12-02 08:50:11 +0800

    曹佳杰 每日趣闻

--- app/src/main/java/com/r_time_run/newmess/subactivity/News_Activity.java
+++ app/src/main/java/com/r_time_run/newmess/subactivity/News_Activity.java
@@ -6,6 +6,7 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -13,10 +14,14 @@ import com.r_time_run.newmess.R;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
+
 public class News_Activity extends AppCompatActivity {
     private ViewPager vpNews;
     private LinearLayout llPointgroup;
     private TextView tvNews;
+    private final int[] imageIDs={R.drawable.news1,R.drawable.news1};
+    private ArrayList<ImageView> imageList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +29,13 @@ public class News_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_news);
         vpNews = (ViewPager) findViewById(R.id.vp_news);
         llPointgroup= (LinearLayout) findViewById(R.id.ll_pointgroup);
+//        初始化图片资源
+        imageList=new ArrayList<ImageView>();
+        for (int i=0;i<imageIDs.length;i++){
+            ImageView image=new ImageView(this);
+            image.setBackgroundResource(imageIDs[i]);
+            imageList.add(image);
+        }
         tvNews = (TextView) findViewById(R.id.tv_news);
         vpNews.setAdapter(new MyPagerAdapter());
     }
@@ -31,22 +43,31 @@ public class News_Activity extends AppCompatActivity {
 
         @Override
         public int getCount() {
-            return 0;
+            return imageList.size();
         }
 
+        /**
+         * 判断view和object的对应关系
+         */
         @Override
         public boolean isViewFromObject(View view, Object object) {
-            return false;
+            return view==object;
         }
 
+        /**
+         * 获得相应位置的view
+         */
         @Override
         public Object instantiateItem(ViewGroup container, int position) {
-            return super.instantiateItem(container, position);
+            container.addView(imageList.get(position));
+            return imageList.get(position);
         }
 
         @Override
         public void destroyItem(ViewGroup container, int position, Object object) {
-            super.destroyItem(container, position, object);
+            container.removeView((View) object);
+            object=null;
+//            super.destroyItem(container, position, object);
         }
     }
 
